9a3262cab576ba23955cab11464b7d83ac74ab63
Gabriel Rodrigues Palma <58814900+GabrielRPalma@users.noreply.github.com>
2022-07-07 19:40:15 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PyPBP'
+copyright = '2022, Gabriel'
+author = 'Gabriel Rodrigues Palma'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.0.2'
 
 # -- General configuration
 
